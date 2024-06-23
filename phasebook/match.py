5047d4106cee02a8dd212d5200142669ce47ac6b
aleacaliste <168704974+aleacaliste@users.noreply.github.com>
2024-06-23 13:22:59 +0800

    bizkit-test

--- phasebook/match.py
+++ phasebook/match.py
@@ -12,16 +12,14 @@ def match(match_id):
     if match_id < 0 or match_id >= len(MATCHES):
         return "Invalid match id", 404
 
-    start = time.time()
+   #using time.perf_counter() provides a more precise timer for measuring short durations.
+    start_time = time.perf_counter()
     msg = "Match found" if (is_match(*MATCHES[match_id])) else "No match"
-    end = time.time()
+    elapsed_time = time.perf_counter() - start_time
 
-    return {"message": msg, "elapsedTime": end - start}, 200
-
+    return {"message": msg, "elapsedTime": elapsed_time}, 200
 
 def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
-
-    return True
+    #fave_numbers_1 is converted to a set fave_numbers_set, making the membership check inside is_match much faster.
+    fave_numbers_set = set(fave_numbers_1)
+    return all(number in fave_numbers_set for number in fave_numbers_2)
--- phasebook/search.py
+++ phasebook/search.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request,jsonify    
 
 from .data.search_data import USERS
 
@@ -27,4 +27,25 @@ def search_users(args):
 
     # Implement search here!
 
-    return USERS
+    priority_order = ["id", "name", "age", "occupation"]
+    
+    # Sorting keys based on priority_order
+    keys = list(args.keys())
+    sorted_keys = [key for key in priority_order if key in keys]
+    final_keys = sorted_keys + [key for key in keys if key not in sorted_keys]
+
+    if final_keys:
+        result = []
+        for key in final_keys:
+            for user in USERS:
+                if key == "age":
+                    # Check if age is within range age - 1 to age + 1
+                    user_age = user["age"]
+                    search_age = int(args["age"])
+                    if user_age >= search_age - 1 and user_age <= search_age + 1:
+                        if user not in result:
+                            result.append(user)
+                elif str(args[key]).lower() in str(user[key]).lower() and user not in result:
+                    result.append(user)
+
+        return result
